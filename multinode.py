03632187eb42e4721ab566f39a4a77b28768f821
Andrew Chung <89956+murkyl@users.noreply.github.com>
2023-08-02 23:34:17 +0800

    Add capability to send config data to newly connected clients Initial client and server processing loops added

--- multinode.py
+++ multinode.py
@@ -16,14 +16,123 @@ import copy
 import logging
 import multiprocessing as mp
 import os
+import platform
 import queue
+import select
 import subprocess
 import time
 
 import libs.hydra as Hydra
 import libs.remote_run as RR
 
+# Global variables to allow direct access to the objects for a signal handler
+client = None
+svr = None
 LOG = logging.getLogger()
+
+
+class PSScanClient(object):
+    def __init__(self, args={}):
+        # TODO: Set default for poll interval
+        self.poll_interval = args.get("poll_interval", 1)
+        self.server_addr = args.get("server_addr", Hydra.DEFAULT_SERVER_ADDR)
+        self.server_port = args.get("server_port", Hydra.DEFAULT_SERVER_PORT)
+        self.socket = Hydra.HydraSocket(
+            {
+                "server_addr": self.server_addr,
+                "server_port": self.server_port,
+            }
+        )
+        self.wait_list = [self.socket]
+
+    def parse_config_update_logger(self, cfg):
+        format_string_vars = {
+            "filename": platform.node(),
+        }
+        try:
+            logger_block = cfg.get("logger")
+            if logger_block["destination"] == "file":
+                log_filename = logger_block["filename"].format(**format_string_vars)
+                log_handler = logging.FileHandler(log_filename)
+                log_handler.setFormatter(logging.Formatter(logger_block["format"]))
+                LOG.handlers[:] = []
+                LOG.addHandler(log_handler)
+                LOG.setLevel(logger_block["level"])
+        except KeyError as ke:
+            print("ERROR: Logger filename string is invalid: {txt}".format(txt=str(ke)))
+        except Exception as e:
+            print("ERROR: Unhandled exception while trying to configure logger: {txt}".format(txt=str(ke)))
+
+    def parse_message(self, msg):
+        msg_type = msg.get("type")
+        if msg_type == "config_update":
+            cfg = msg.get("config")
+            if "logger" in cfg:
+                self.parse_config_update_logger(cfg)
+
+    def connect(self):
+        LOG.info("Connecting to server at {svr}:{port}".format(svr=self.server_addr, port=self.server_port))
+        connected = self.socket.connect()
+        if not connected:
+            LOG.info("Unable to connect to server")
+            return
+        continue_running = True
+        while continue_running:
+            rlist, _, xlist = select.select(self.wait_list, [], self.wait_list, self.poll_interval)
+            if rlist:
+                data = self.socket.recv()
+                msg_type = data.get("type")
+                if msg_type == "cmd":
+                    cmd = data.get("cmd")
+                    LOG.debug("Command received: {cmd}".format(cmd=cmd))
+                    if cmd == "closed":
+                        self.wait_list.remove(self.socket)
+                        self.socket.disconnect()
+                        continue_running = False
+                        continue
+                elif msg_type == "data":
+                    msg_data = data.get("data")
+                    self.parse_message(msg_data)
+                elif msg_type is None:
+                    LOG.debug("Socket ready to read but no data was received. We should shutdown now.")
+                    self.wait_list.remove(self.socket)
+                    self.socket.disconnect()
+                    continue_running = False
+                else:
+                    LOG.debug("Unexpected message received: {data}".format(data=data))
+            if xlist:
+                LOG.error("Socket encountered an error or was closed")
+                self.wait_list.remove(self.socket)
+                continue_running = False
+                break
+
+
+class PSScanCommandClient(object):
+    def __init__(self, args={}):
+        self.commands = args.get("commands", [])
+        self.server_addr = args.get("server_addr", Hydra.DEFAULT_SERVER_ADDR)
+        self.server_port = args.get("server_port", Hydra.DEFAULT_SERVER_PORT)
+        self.socket = Hydra.HydraSocket(
+            {
+                "server_addr": self.server_addr,
+                "server_port": self.server_port,
+            }
+        )
+
+    def connect(self):
+        if not self.commands:
+            LOG.info("No commands to send. No connection to server required.")
+            return
+        LOG.info("Connecting to server at {svr}:{port}".format(svr=self.server_addr, port=self.server_port))
+        connected = self.socket.connect()
+        if not connected:
+            LOG.info("Unable to connect to server")
+            return
+        if self.commands[0] == "quit":
+            LOG.info('Sending "quit" command to server')
+            self.socket.send({"type": "cmd", "cmd": "quit"})
+        time.sleep(1)
+        self.socket.disconnect()
 
 
 class PSScanServer(Hydra.HydraServer):
@@ -33,9 +142,9 @@ class PSScanServer(Hydra.HydraServer):
         self.connect_addr = args.get("server_connect_addr", None)
         self.message_queue = queue.Queue()
         self.node_list = args.get("node_list", None)
-        self.proc_list = []
         self.remote_state = None
         self.script_path = args.get("script_path", None)
+        self.work_queue = queue.Queue()
         if not (self.connect_addr and self.script_path):
             raise Exception("Server connect address and script path is required")
 
@@ -43,16 +152,14 @@ class PSScanServer(Hydra.HydraServer):
         """
         Users should override this method to add their own handler for client commands.
         """
-        # LOG.debug("Client {addr} command data: {data}".format(addr=self.clients[client]["client_address"], data=msg))
         if not msg or (msg["type"] == "cmd" and msg["cmd"] == "closed"):
-            self.message_queue.put({"type": "client_closed", "data": msg, "client": client})
+            self.message_queue.put({"type": "control_client_closed", "data": msg, "client": client})
             self._remove_client(client)
         elif msg["type"] == "data":
-            self.message_queue.put({"type": "client_command", "data": msg["data"], "client": client})
+            self.message_queue.put({"type": "client_data", "data": msg["data"], "client": client})
 
     def handler_client_connect(self, client):
-        # LOG.debug("Client {addr} just connected".format(addr=client))
-        self.message_queue.put({"type": "client_connect", "client": client})
+        self.message_queue.put({"type": "control_client_connect", "client": client})
 
     def launch_remote_processes(self):
         remote_server_addr = self.connect_addr
@@ -75,24 +182,62 @@ class PSScanServer(Hydra.HydraServer):
             self.remote_state.connect(self.node_list)
 
     def parse_message(self, msg):
-        if msg["type"] == "quit":
-            LOG.debug("Quit command received: %s" % msg)
-        elif msg["type"] == "client_closed":
-            LOG.debug("Client socket closed: %s" % msg)
-        elif msg["type"] == "client_command":
-            LOG.debug("Client socket command: %s" % msg)
-        elif msg["type"] == "client_connect":
-            LOG.debug("Client socket connected: %s" % msg)
-        elif msg["type"] == "remote_msg":
+        if msg["type"] == "client_data":
+            LOG.debug("Client data: {data}".format(data=msg))
+            data = msg["data"]
+            data_type = data.get("type")
+            if data_type == "cmd":
+                cmd = data.get("cmd")
+                if cmd == "quit":
+                    LOG.debug("Quit command received: {data}".format(data=data))
+                    return {"cmd": "quit"}
+            elif data_type == "dir_list":
+                pass
+        elif msg["type"] == "control_client_closed":
+            LOG.debug("Client socket closed: {data}".format(data=msg))
+        elif msg["type"] == "control_client_connect":
+            LOG.debug("Client socket connected: {data}".format(data=msg))
+            # Send configuration to client
+            self.send(
+                msg["client"],
+                {
+                    "type": "config_update",
+                    "config": {
+                        "logger": {
+                            "format": "%(asctime)s - %(levelname)s - [%(module)s:%(lineno)d] - (%(process)d|%(threadName)s) %(message)s",
+                            "destination": "file",
+                            "filename": "log-{filename}.txt",
+                            "level": LOG.level,
+                        }
+                    },
+                },
+            )
+            # Send up to 1 directory in our work queue to each connected client
+            if not self.work_queue.empty():
+                try:
+                    work_item = self.work_queue.get(False)
+                    self.send(
+                        msg["client"],
+                        {
+                            "type": "dir_list",
+                            "work_item": [work_item],
+                        },
+                    )
+                except queue.Empty as qe:
+                    LOG.error("Work queue was not empty but unable to get a work item to send to the new client")
+        elif msg["type"] == "control_remote_msg":
             # This type of command is sent from the remote_run module that handles spawning processes on other machines
             # TODO: Add code to handle re-launching dead processes
-            LOG.debug("Remote process message: %s" % msg)
+            # TODO: Log any console output if there is an error
+            LOG.debug(
+                "Remote process message from client ID {client}: {data}".format(client=msg["client_id"], data=msg)
+            )
         else:
-            LOG.debug("MSG is: %s" % msg)
+            LOG.debug("MSG is: {data}".format(data=msg))
+        return {}
 
     def remote_callback(self, client, client_id, msg=None):
-        # LOG.debug("Remote client {client_id} returned message: {msg}".format(client_id=client_id, msg=msg))
-        self.message_queue.put({"type": "remote_msg", "data": msg, "client_id": client_id, "client": client})
+        self.message_queue.put({"type": "control_remote_msg", "data": msg, "client_id": client_id, "client": client})
 
     def serve(self):
         LOG.info("Starting server")
@@ -101,90 +246,53 @@ class PSScanServer(Hydra.HydraServer):
         self.launch_remote_processes()
         # Start main processing loop, send initial directories to process to clients
         # Wait for clients to request work and redistribute work as needed.
-
         try:
+            # TODO: Set queue timeout
+            queue_timeout = 1
             continue_running = True
             while continue_running:
                 now = time.time()
                 try:
-                    queue_item = self.message_queue.get(timeout=1)
+                    queue_item = self.message_queue.get(timeout=queue_timeout)
                 except queue.Empty as qe:
                     queue_item = None
                 if queue_item:
-                    self.parse_message(queue_item)
-                LOG.debug("CHECK!")
-                if now - start_time > 10:
-                    continue_running = False
+                    reponse = self.parse_message(queue_item)
+                    if reponse.get("cmd") == "quit":
+                        continue_running = False
+                LOG.debug("TICK!")
+                # if now - start_time > 20:
+                #    continue_running = False
         except Exception as e:
             LOG.exception(e)
-        """
-        print("WAIT FOR PROC COMPLETE")
-        for i in range(100):
-            print("i is: %s" % i)
-            remove_list = []
-            for remote_entry in remote_list:
-                poll_result = remote_entry["proc"].poll()
-                if poll_result is not None:
-                    print("Command returned: %s" % remote_entry)
-                    remove_list.append(remote_entry)
-                else:
-                    print("Poll result: %s" % poll_result)
-            for x in remove_list:
-                remote_list.remove(x)
-            if not remote_list:
-                print("Breaking out")
-                break
-            time.sleep(1)
-        """
-        LOG.debug("ps_scan coordinator exiting")
+        LOG.debug("ps_scan shutting down")
         self.shutdown()
+        LOG.debug("ps_scan shutdown complete")
 
-
-class PSScanClient(object):
-    def __init__(self, args={}):
-        self.server_addr = args.get("server_addr", Hydra.DEFAULT_SERVER_ADDR)
-        self.server_port = args.get("server_port", Hydra.DEFAULT_SERVER_PORT)
-        self.test_cases = args.get("test_cases", [1])
-
-    def connect(self):
-        LOG.info("Connecting to server at {svr}:{port}".format(svr=self.server_addr, port=self.server_port))
-
-        """
-        Test case of a normal connect, send, then disconnect
-        """
-        if 1 in self.test_cases:
-            test_client = Hydra.HydraSocket(
-                {
-                    "server_addr": self.server_addr,
-                    "server_port": self.server_port,
-                }
-            )
-            connected = test_client.connect()
-            if not connected:
-                LOG.info("Unable to connect to server")
-            test_client.send({"some": "dict"})
-            LOG.info("Sleeping 1 second")
-            time.sleep(1)
-            test_client.disconnect()
 
 def get_local_internal_addr():
     # TODO: Add checks for running on OneFS and other OSes
     subproc = subprocess.Popen(
-        ["isi_nodes", "-L", "\"%{internal}\""],
+        ["isi_nodes", "-L", '"%{internal}"'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
     stdout, stderr = subproc.communicate()
-    addr = stdout.strip().replace("\"", "")
+    addr = stdout.strip().replace('"', "")
     return addr
+
 
 def get_script_path():
     return os.path.abspath(__file__)
 
+
 def main():
+    global client
+    global svr
     import optparse
     import sys
 
+    # TODO: Need to get this from config file or CLI options
     node_list = [
         {
             "endpoint": "5",
@@ -203,9 +311,9 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option("--port", action="store", type="int", default=Hydra.DEFAULT_SERVER_PORT)
     parser.add_option("--addr", action="store", default=Hydra.DEFAULT_SERVER_ADDR)
-    parser.add_option("--test", action="append", type="int", default=[])
-    if len(sys.argv) < 2 or args[0] not in ("client", "server", "auto"):
-        print("{prog} [--port=#] [--addr=#] <server|client|auto>".format(prog=sys.argv[0]))
+    parser.add_option("--cmd", action="append", type="str", default=[])
+    if len(sys.argv) < 2 or sys.argv[1] not in ("auto", "client", "command", "server"):
+        print("{prog} [--port=#] [--addr=#] <auto|client|command|server>".format(prog=sys.argv[0]))
         sys.exit(1)
     (options, args) = parser.parse_args(sys.argv[1:])
 
@@ -214,7 +322,6 @@ def main():
         "server_port": options.port,
         "server_addr": options.addr,
         "server_connect_addr": get_local_internal_addr(),
-        # TODO: Need to get this from config file or CLI options
         "node_list": None,
     }
 
@@ -222,13 +329,21 @@ def main():
         LOG.info("Starting client")
         client = PSScanClient({"server_port": options.port, "server_addr": options.addr})
         client.connect()
-    elif args[0] == "server":
+    elif args[0] == "command":
+        LOG.info("Sending command to server")
+        client = PSScanCommandClient(
+            {
+                "commands": options.cmd,
+                "server_port": options.port,
+                "server_addr": options.addr,
+            }
+        )
+        client.connect()
+    elif args[0] in ("auto", "server"):
         LOG.info("Starting server")
-        svr = PSScanServer(ps_scan_server_options)
-        svr.serve()
-    elif args[0] == "auto":
-        LOG.info("Starting server")
-        ps_scan_server_options["node_list"] = node_list
+        if args[0] == "auto":
+            # Setting the node list will cause the server to automatically launch clients
+            ps_scan_server_options["node_list"] = node_list
         svr = PSScanServer(ps_scan_server_options)
         svr.serve()
 
