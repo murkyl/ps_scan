105dad711ab4ffb877dd1be052106874ded8de15
Andrew Chung <89956+murkyl@users.noreply.github.com>
2023-08-21 23:04:45 +0800

    Add visualization creation for local storagepool usage

---
+++ visualizations/gen_local_storagepool_usage_ndjson.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+This script can be run directly to replace the existing access zone filters in a
+Kibana saved object
+"""
+# fmt: off
+__title__         = "gen_local_storagepool_usage_ndjson"
+__version__       = "1.0.0"
+__date__          = "21 August 2023"
+__license__       = "MIT"
+__author__        = "Andrew Chung <andrew.chung@dell.com>"
+__maintainer__    = "Andrew Chung <andrew.chung@dell.com>"
+__email__         = "andrew.chung@dell.com"
+# fmt: on
+
+
+def main():
+    import sys
+    from helpers.vis_gen_helpers import generate_local_storagepool_usage_ndjson
+
+    num_args = len(sys.argv)
+    if num_args < 2 or num_args > 3:
+        sys.stderr.write("Usage: {prog} <input_template> [<output_file_defaults_to_stdout>]\n".format(prog=sys.argv[0]))
+        sys.exit(1)
+    infile = open(sys.argv[1], "r")
+    outfile = sys.stdout if num_args == 2 else open(sys.argv[2], "w")
+    template_array = infile.readlines()
+    infile.close()
+    ndjson_array = generate_local_storagepool_usage_ndjson(template_array)
+    for line in ndjson_array:
+        outfile.write(line + "\n")
+    outfile.close()
+
+
+if __name__ == "__main__" or __file__ == None:
+    main()
